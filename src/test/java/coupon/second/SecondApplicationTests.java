d2949e9d593b1b41cce085d605834c7fa7bfc68e
km1031kim@naver.com <km1031kim@naver.com>
2025-10-07 00:14:17 +0900

    first commit

---
+++ src/test/java/coupon/second/SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package coupon.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
