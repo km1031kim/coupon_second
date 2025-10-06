21da99a841c6cce9014496841909f8d0a7708bc5
km1031kim@naver.com <km1031kim@naver.com>
2025-10-07 00:37:10 +0900

    [COMMON] 1. SecurityConfig 작성. permitAll() 설정 2. 패키지 구조 설정 coupon.api.dto -> application layer dto coupon.service.dto -> service layer dto 서비스 계층이 프레젠테이션 계층에 의존하지 않도록 dto 분리 (효율적인 방법인지는.. 써보면서 확인하자)

---
+++ src/main/java/coupon/second/common/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package coupon.second.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
